5c115a0f7990a6e080782046ed11e9a4226b1731
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-12 18:12:38 +0200

    remove custom SafeParcel implementation
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/app/grapheneos/gmscompat/safeparcel/SafeParcelable.java
+++
@@ -1,5 +0,0 @@
-package app.grapheneos.gmscompat.safeparcel;
-
-import android.os.Parcelable;
-
-abstract class SafeParcelable implements Parcelable {}
--- src/app/grapheneos/gmscompat/safeparcel/SpWriteOnly.java
+++
@@ -1,7 +0,0 @@
-package app.grapheneos.gmscompat.safeparcel;
-
-public abstract class SpWriteOnly extends SafeParcelable {
-    public int describeContents() {
-        return 0;
-    }
-}
