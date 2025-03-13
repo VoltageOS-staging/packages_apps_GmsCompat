bcde8de1d89d46404b6210bf2f7384f693364555
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-13 18:27:27 +0200

    Revert "location service: remove requestLocationSettingsDialog() implementation"
    This change led to app compatibility issues, since Location Accuracy toggle started to impact
    requestLocationSettingsDialog() result.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/app/grapheneos/gmscompat/location/GLocationService.kt
+++ src/app/grapheneos/gmscompat/location/GLocationService.kt
@@ -1,6 +1,8 @@
 package app.grapheneos.gmscompat.location
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.location.Location
 import android.location.LocationManager
 import android.os.CancellationSignal
@@ -9,6 +11,7 @@ import android.os.RemoteException
 import androidx.annotation.Keep
 import app.grapheneos.gmscompat.BinderDefSupplier
 import app.grapheneos.gmscompat.logd
+import com.android.internal.gmscompat.GmsCompatApp
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.common.api.internal.IStatusCallback
@@ -20,12 +23,16 @@ import com.google.android.gms.location.LastLocationRequest
 import com.google.android.gms.location.LocationAvailability
 import com.google.android.gms.location.LocationAvailabilityRequest
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResult
+import com.google.android.gms.location.LocationSettingsStates
 import com.google.android.gms.location.internal.FusedLocationProviderResult
 import com.google.android.gms.location.internal.IBooleanStatusCallback
 import com.google.android.gms.location.internal.IFusedLocationProviderCallback
 import com.google.android.gms.location.internal.IGoogleLocationManagerService
 import com.google.android.gms.location.internal.ILocationAvailabilityStatusCallback
 import com.google.android.gms.location.internal.ILocationStatusCallback
+import com.google.android.gms.location.internal.ISettingsCallbacks
 import com.google.android.gms.location.internal.LocationReceiver
 import com.google.android.gms.location.internal.LocationRequestUpdateData
 import java.util.concurrent.Executors
@@ -265,6 +272,40 @@ class GLocationService(val ctx: Context) : IGoogleLocationManagerService.Stub() 
         callback.onResult(Status.SUCCESS, res)
     }
 
+    // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient#checkLocationSettings(com.google.android.gms.location.LocationSettingsRequest)
+    override fun requestLocationSettingsDialog(
+        settingsRequest: LocationSettingsRequest,
+        callback: ISettingsCallbacks,
+        packageName: String?
+    ) {
+        logd{"$settingsRequest, packageName $packageName"}
+        // GmsCore doesn't check whether caller has a location permission in this case
+
+        val states = LocationSettingsStates().apply {
+            val lm = nonClientLocationManager
+            gpsPresent = lm.hasProvider(LocationManager.GPS_PROVIDER)
+            gpsUsable = gpsPresent && lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            networkLocationPresent = lm.hasProvider(LocationManager.NETWORK_PROVIDER)
+            networkLocationUsable = networkLocationPresent && lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        }
+
+        val status = if (states.gpsUsable || states.networkLocationUsable) {
+            Status.SUCCESS
+        } else {
+            Status(CommonStatusCodes.RESOLUTION_REQUIRED).apply {
+                val intent = Intent().apply {
+                    // StubResolutionActivity is needed for compatibility with apps that expect the
+                    // RESOLUTION_REQUIRED status when global location toggle or network location
+                    // provider is off
+                    setClassName(GmsCompatApp.PKG_NAME, StubResolutionActivity::class.java.name)
+                }
+                resolution = PendingIntent.getActivity(ctx, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+                statusMessage = "no usable location providers"
+            }
+        }
+        callback.onLocationSettingsResult(LocationSettingsResult(status, states))
+    }
+
     // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient#public-abstract-taskboolean-isgooglelocationaccuracyenabled
     override fun isGoogleLocationAccuracyEnabled(callback: IBooleanStatusCallback) {
         logd{}
@@ -284,6 +325,7 @@ class GLocationService(val ctx: Context) : IGoogleLocationManagerService.Stub() 
             /* getLastLocation5 */ 90,
             /* getLocationAvailability */ 34,
             /* getLocationAvailability2 */ 91,
+            /* requestLocationSettingsDialog */ 63,
             /* updateLocationRequest */ 59,
             /* flushLocations */ 67,
             /* registerLocationReceiver */ 88,
